9a6ce50ed29d6fee6cc57b00a5b50969a3dd5e79
Jess <55003055+JesseBadgley@users.noreply.github.com>
2019-11-15 17:27:21 -0600

    UI update, finished redux.

---
+++ src/redux/reducers/uiReducer.js
@@ -0,0 +1,40 @@
+import {
+  SET_ERRORS,
+  CLEAR_ERRORS,
+  LOADING_UI,
+  LOADING_UI_COMPLETED
+} from '../types';
+
+const initialState = {
+  loading: false,
+  errors: null
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case SET_ERRORS:
+      return {
+        ...state,
+        loading: false,
+        errors: action.payload
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        loading: false,
+        errors: null
+      };
+    case LOADING_UI:
+      return {
+        ...state,
+        loading: true
+      };
+    case LOADING_UI_COMPLETED:
+      return {
+        ...state,
+        loading: false
+      };
+    default:
+      return state;
+  }
+}
