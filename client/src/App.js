4859c624e6eba66068a7dc6ff186676f4890a2bb
Jesse Badgley <55003055+JesseBadgley@users.noreply.github.com>
2019-12-01 18:26:14 -0600

    Add files via upload
    finished client

--- client/src/App.js
+++ client/src/App.js
@@ -4,29 +4,40 @@ import './App.css';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 import jwtDecode from 'jwt-decode';
+import axios from 'axios';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { SET_AUTHENTICATED } from './redux/types';
+import { logoutUser, getUserData } from './redux/actions/userActions';
 
 // Components
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import user from './components/user';
 
 //Util
 import AuthRoute from './util/AuthRoute';
+// import themeObject from './util/theme';
 
-// Pages
-import home from './pages/home';
-import login from './pages/login';
-import signup from './pages/signup';
-
-let authenticated;
+// let authenticated;
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
   if (decodedToken.exp * 1000 < Date.now()) {
+    store.dispatch(logoutUser());
     window.location.href = '/login';
-    authenticated = false;
   } else {
-    authenticated = true;
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common['Authorization'] = token;
+    store.dispatch(getUserData());
   }
 }
+
+// const theme = createMuiTheme(themeObject);
 
 const theme = createMuiTheme({
   palette: {
@@ -79,28 +90,25 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <div className='App'>
+        <Provider store={store}>
           <Router>
             <Navbar />
             <div className='container'>
               <Switch>
-                <Route exact path='/' component={home} />
-                <AuthRoute
+                <Route exact path='/' component={Home} />
+                <AuthRoute exact path='/login' component={Login} />
+                <AuthRoute exact path='/signup' component={Signup} />
+                <Route exact path='/users/:handle' component={user} />
+                <Route
                   exact
-                  path='/login'
-                  component={login}
-                  authenticated={authenticated}
+                  path='/users/:handle/meow/:meowId'
+                  component={user}
                 />
-                <AuthRoute
-                  exact
-                  path='/signup'
-                  component={signup}
-                  authenticated={authenticated}
-                />
+                >
               </Switch>
             </div>
           </Router>
-        </div>
+        </Provider>
       </MuiThemeProvider>
     );
   }
--- client/src/util/theme.js
+++ client/src/util/theme.js
@@ -1,20 +1,20 @@
 export default {
   palette: {
     primary: {
-      light: '#8bf6ff',
-      main: '#4fc3f7',
-      dark: '#0093c4',
-      contrastText: '#000'
+      light: '#33c9dc',
+      main: '#00bcd4',
+      dark: '#008394',
+      contrastText: '#fff'
     },
     secondary: {
-      light: '#efefef',
-      main: '#bdbdbd',
-      dark: '#8d8d8d',
-      contrastText: '#000'
+      light: '#ff6333',
+      main: '#ff3d00',
+      dark: '#b22a00',
+      contrastText: '#fff'
     }
   },
   typography: {
-    useNextVarients: true
+    useNextVariants: true
   },
   form: {
     textAlign: 'center'
@@ -29,18 +29,69 @@ export default {
     margin: '10px auto 10px auto'
   },
   button: {
-    marginTop: '20px',
+    marginTop: 20,
     position: 'relative'
   },
   customError: {
     color: 'red',
     fontSize: '0.8rem',
-    marginTop: '12px'
-  },
-  small: {
-    marginTop: '10px'
+    marginTop: 10
   },
   progress: {
     position: 'absolute'
+  },
+  invisibleSeparator: {
+    border: 'none',
+    margin: 4
+  },
+  visibleSeparator: {
+    width: '100%',
+    borderBottom: '1px solid rgba(0,0,0,0.1)',
+    marginBottom: 20
+  },
+  paper: {
+    padding: 20
+  },
+  profile: {
+    '& .image-wrapper': {
+      textAlign: 'center',
+      position: 'relative',
+      '& button': {
+        position: 'absolute',
+        top: '80%',
+        left: '70%'
+      }
+    },
+    '& .profile-image': {
+      width: 200,
+      height: 200,
+      objectFit: 'cover',
+      maxWidth: '100%',
+      borderRadius: '50%'
+    },
+    '& .profile-details': {
+      textAlign: 'center',
+      '& span, svg': {
+        verticalAlign: 'middle'
+      },
+      '& a': {
+        color: '#00bcd4'
+      }
+    },
+    '& hr': {
+      border: 'none',
+      margin: '0 0 10px 0'
+    },
+    '& svg.button': {
+      '&:hover': {
+        cursor: 'pointer'
+      }
+    }
+  },
+  buttons: {
+    textAlign: 'center',
+    '& a': {
+      margin: '20px 10px'
+    }
   }
 };
