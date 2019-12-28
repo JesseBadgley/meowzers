2af798ccef26950d8a2d7ced1a8429ae5be39d5d
HAL_9000 <jessebadgley@HAL-9000.local>
2019-12-27 19:15:54 -0600

    finished and hosted

---
+++ build/precache-manifest.e5df72142e4b7913bedd20d0364f374a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4a764a197b06de84ceb0d00a737466",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc5405c0f9ef9ddf2ac2",
+    "url": "/static/css/main.2a700a44.chunk.css"
+  },
+  {
+    "revision": "b1b3930ab193d299ba0f",
+    "url": "/static/js/2.591b3401.chunk.js"
+  },
+  {
+    "revision": "dc5405c0f9ef9ddf2ac2",
+    "url": "/static/js/main.2079e79b.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/static/media/no-img.5a021ab9.png"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -38,6 +38,8 @@ if (token) {
 	}
 }
 
+// const theme = createMuiTheme(themeObject);
+
 const theme = createMuiTheme({
 	palette: {
 		primary: {
